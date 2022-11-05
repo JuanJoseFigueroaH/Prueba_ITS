176661636a6120cb5acbc26869e347c34e96f8e5
Juan Jose Figueroa <jjfiguer@bancolombia.com.co>
2022-11-04 19:46:18 -0500

    Creation of login and registration view, also creation of auth service

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import Login from './components/auth/login'
+import Register from './components/auth/register'
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="register" element={<Register />} />
     </Routes>
   );
 }
